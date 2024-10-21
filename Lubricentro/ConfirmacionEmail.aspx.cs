afc3b6234dee67136ab51dcfe0fec60adc35c8f8
mateo yañez <PS4matupulga@yahoo.com>
2024-10-21 18:44:22 -0300

    Confirmacion del mail, solo la verificacion

--- Lubricentro/ConfirmacionEmail.aspx.cs
+++ Lubricentro/ConfirmacionEmail.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Net.Mail;
 
 namespace Lubricentro
 {
@@ -11,7 +12,31 @@ namespace Lubricentro
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)  // Verifica que la página no se esté recargando por un postback
+            {
+                // Obtén el token de la URL
+                string token = Request.QueryString["token"];
 
+                if (!string.IsNullOrEmpty(token))
+                {
+                    // Llama a un método para verificar el token y confirmar el email
+                    if (ConfirmarEmail(token))
+                    {
+                        // Si el token es válido y el email fue confirmado
+                        Response.Write("<h2>¡Tu email ha sido confirmado exitosamente!</h2>");
+                    }
+                    else
+                    {
+                        // Si el token no es válido o ya expiró
+                        Response.Write("<h2>Error: El token es inválido o ya ha sido utilizado.</h2>");
+                    }
+                }
+                else
+                {
+                    Response.Write("<h2>Error: No se ha proporcionado un token de confirmación.</h2>");
+                }
+            }
         }
+    }
     }
 }
